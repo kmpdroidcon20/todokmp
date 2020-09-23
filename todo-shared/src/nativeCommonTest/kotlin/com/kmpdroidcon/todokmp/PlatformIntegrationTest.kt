a29a9b1c502f65838c630537931afed21b22e39d
Marco Signoretto <marco.signoretto.dev@gmail.com>
2020-09-23 09:14:40 +0400

    Base classes for integration tests

---
+++ todo-shared/src/nativeCommonTest/kotlin/com/kmpdroidcon/todokmp/PlatformIntegrationTest.kt
@@ -0,0 +1,14 @@
+package com.kmpdroidcon.todokmp
+
+/**
+ * Created by
+ * Marco Signoretto
+ * Android Developer
+ * on 23/09/2020.
+ */
+
+actual abstract class PlatformIntegrationTest {
+    private val testInjector = TestInjector()
+    actual fun testInjector(): TestInjector = testInjector
+    actual open fun setup() {}
+}
