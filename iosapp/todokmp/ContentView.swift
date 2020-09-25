ed30e55e7c94a0d74ab419b6930e8f7f25d43379
Marco Signoretto <marco.signoretto.dev@gmail.com>
2020-09-25 09:13:31 +0400

    SwiftUi listview (#19)

--- iosapp/todokmp/ContentView.swift
+++ iosapp/todokmp/ContentView.swift
@@ -9,10 +9,26 @@
 import SwiftUI
 import TodoCommon
 
+struct TodoSwiftUiItem: Identifiable {
+    var id = UUID()
+    var timestamp: String
+    var content: String
+}
+
 struct ContentView: View {
-    let repo : TodoListRepository? = nil // TODO remove this
+    // TODO make this one subscribe to shared ViewModel
+    let modelData : [TodoSwiftUiItem] = [
+        TodoUiItem(timestamp: "12/12/2020 12:40", content: "Todo 1"),
+        TodoUiItem(timestamp: "12/12/2020 12:50", content: "Todo 2"),
+        ].map{item in TodoSwiftUiItem(timestamp: item.timestamp, content: item.content)}
+
     var body: some View {
-        Text("Hello, World!")
+        List(modelData) { item in
+            VStack {
+                Text(item.timestamp)
+                Text(item.content)
+            }
+        }
     }
 }
 
