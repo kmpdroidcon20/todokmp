c33a52c9d3c48a562defd62564657a332ab69a5d
Marco Signoretto <marco.signoretto@careem.com>
2020-10-02 16:43:02 +0400

    Added class to reproduce Invalid Mutability Issue

--- todo-inmemory-repository/src/commonMain/kotlin/com/kmpdroidcon/todokmp/datasource/di/TodoInMemoryDataSourceModule.kt
+++ todo-inmemory-repository/src/commonMain/kotlin/com/kmpdroidcon/todokmp/datasource/di/TodoInMemoryDataSourceModule.kt
@@ -1,10 +1,15 @@
 package com.kmpdroidcon.todokmp.datasource.di
 
 import com.kmpdroidcon.todokmp.data.dependency.InMemoryTodoDataSource
+import com.kmpdroidcon.todokmp.datasource.BrokenInMemoryTodoDataSourceImpl
 import com.kmpdroidcon.todokmp.datasource.InMemoryTodoDataSourceImpl
 
 object TodoInMemoryDataSourceModule {
     fun providesInMemoryTodoDataSource(): InMemoryTodoDataSource {
         return InMemoryTodoDataSourceImpl()
+    }
+
+    fun providesBrokenInMemoryTodoDataSource(): InMemoryTodoDataSource {
+        return BrokenInMemoryTodoDataSourceImpl()
     }
 }
